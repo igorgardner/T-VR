9d11111b919a79b141858cb588b65322d9465676
Master PC <EDAS@EDAS>
2019-04-03 16:09:13 +0100

    Gestures impemented need to improve tracking
    Gestures impemented need to improve tracking

---
+++ Unity/Environment/Assets/GearStick.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearStick : MonoBehaviour {
+
+    public GameObject stick;
+    public GameObject axis;
+    public int turnSpeed;
+    public int position;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void MoveForward()
+    {
+        if (position < 5)
+        {
+            stick.transform.RotateAround(axis.transform.position, Vector3.right, 10);
+            position++;
+        }
+    }
+
+    public void MoveBack()
+    {
+        if (position > -5)
+        {
+            stick.transform.RotateAround(axis.transform.position, -Vector3.right, 10);
+            position--;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            while(Input.GetKey(KeyCode.W))
+            {
+                stick.transform.RotateAround(axis.transform.position, Vector3.right, 5);
+                position++;
+
+                if (position>5)
+                {
+                    
+                }
+            }
+            
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            while(Input.GetKey(KeyCode.S))
+            {
+                stick.transform.RotateAround(axis.transform.position, -Vector3.right, 5);
+                position--;
+
+                if (position < -5)
+                {
+
+                }
+            }
+        }
+    }
+}
