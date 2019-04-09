5b4c4d40f812ae52af7653aa1163b3e3f4d383e3
igorgardner <47455499+igorgardner@users.noreply.github.com>
2019-04-09 13:13:18 +0100

    serial updated

--- Arduino/serialArd/serialArd.ino
+++ Arduino/serialArd/serialArd.ino
@@ -1,44 +1,41 @@
-#include <SoftwareSerial.h>
-#include <SerialCommand.h>
-
 int fwdPin = 5;
 int bckPin = 6;
 int lftPin = 4;
 int rgtPin = 7;
 
-SerialCommand input;
+String input;
 
-void fwd1(const char *command)
+void fwd1()
 {
   analogWrite(fwdPin, 140);
   digitalWrite(bckPin, 1);
 }
-void fwd2(const char *command)
+void fwd2()
 {
   digitalWrite(fwdPin, 0);
   digitalWrite(bckPin, 1);
 }
-void stp(const char *command)
+void stp()
 {
   digitalWrite(fwdPin, 1);
   digitalWrite(bckPin, 1);
 }
-void bck(const char *command)
+void bck()
 {
   analogWrite(bckPin, 140);
   digitalWrite(fwdPin, 1);
 }
-void left(const char *command)
+void left()
 {
   digitalWrite(lftPin, 0);
   digitalWrite(rgtPin, 1);
 }
-void right(const char *command)
+void right()
 {
   digitalWrite(lftPin, 1);
   digitalWrite(rgtPin, 0);
 }
-void centre(const char *command)
+void centre()
 {
   digitalWrite(lftPin, 1);
   digitalWrite(rgtPin, 1);
@@ -48,14 +45,6 @@ void setup()
 {
   Serial.begin(9600);
   while(!Serial){;}
-
-  input.addCommand("Speed: 2", fwd2);
-  input.addCommand("Speed: 1", fwd1);
-  input.addCommand("Speed: 0", stp);
-  input.addCommand("Speed: -1", bck);
-  input.addCommand("Steer: 1", left);
-  input.addCommand("Steer: 0", centre);
-  input.addCommand("Steer: -1", right);
     
   pinMode(fwdPin, OUTPUT);
   pinMode(bckPin, OUTPUT);
@@ -71,5 +60,22 @@ void setup()
 void loop() 
 {
   if(Serial.available() > 0)
-    input.readSerial(); 
+  {
+    input = Serial.readString();
+
+    if(input.indexOf("Speed: 2") != -1)
+      fwd2();
+    if(input.indexOf("Speed: 1") != -1)
+     fwd1();
+    if(input.indexOf("Speed: 0") != -1)
+      stp();
+    if(input.indexOf("Speed: -1") != -1)
+     bck();
+    if(input.indexOf("Steer: 1") != -1)
+     left();
+    if(input.indexOf("Steer: 0") != -1)
+     centre();
+    if(input.indexOf("Steer: -1") != -1)
+     right();
+  }
 }
